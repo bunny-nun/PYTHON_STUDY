7e1dcf5dc8d68ce977f3a2ae68c4243bb4a8d92e
Valeria <nelera.gb@gmail.com>
2023-02-06 02:18:23 +0300

    Вычислить число Пи c заданной точностью d

--- PYTHON_HOMEWORK/05.02.2023/task001.py
+++ PYTHON_HOMEWORK/05.02.2023/task001.py
@@ -5,26 +5,28 @@
 
 - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 """
+import time
 
 
 def calculate_pi(d):
 
-    pi = 3
+    start = time.time()
+    pi = 4 * (1 - 1 / 3)
     counter = 0
     i = 2
     dif = 1
     while dif > float(d):
         my_list = list()
-        my_list.append(4 / (i * (i + 1) * (i + 2)))
-        my_list.append(4 / ((i + 2) * (i + 3) * (i + 4)))
+        my_list.append(4 * (-1) ** i / (2 * i + 1))
+        i += 1
+        my_list.append(4 * (-1) ** i / (2 * i + 1))
+        i += 1
         dif = abs(my_list[0] - my_list[1])
-        pi += my_list[0] - my_list[1]
+        pi += my_list[0] + my_list[1]
         counter += 1
-        i += 4
-        print(counter, pi)
     pi_txt = str(pi)
-    print(d, len(d))
-    print(pi_txt[:len(d)])
+    stop = time.time()
+    print(pi_txt[:len(d)], counter, round(stop - start, 2))
 
 
-calculate_pi('0.000001')
+calculate_pi('0.0000001')
