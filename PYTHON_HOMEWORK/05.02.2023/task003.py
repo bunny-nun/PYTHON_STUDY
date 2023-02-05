2bd2cceec253f7916105195c31aa9b4f62b5711d
Valeria <nelera.gb@gmail.com>
2023-02-05 16:11:13 +0300

    Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности

--- PYTHON_HOMEWORK/05.02.2023/task003.py
+++ PYTHON_HOMEWORK/05.02.2023/task003.py
@@ -3,11 +3,16 @@
 список неповторяющихся элементов исходной последовательности
 """
 
-numbers = [1, 6, 7, 8, 9, 0, 8]
+numbers = [1, 6, 7, 1, 8, 9, 0, 8]
 
 
-def unique(lis):
-    return set(lis)
+def unique(lst):
+    new_list = list()
+    for i in lst:
+        if i not in new_list:
+            new_list.append(i)
+
+    return new_list
 
 
-print(*unique(numbers))
+print(unique(numbers))
