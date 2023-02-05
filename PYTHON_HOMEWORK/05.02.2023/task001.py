5edc3a991cc0544fd12ea0697c739d26a41ecd1d
Valeria <nelera.gb@gmail.com>
2023-02-05 14:36:31 +0300

    Вычислить число Пи c заданной точностью d

--- PYTHON_HOMEWORK/05.02.2023/task001.py
+++ PYTHON_HOMEWORK/05.02.2023/task001.py
@@ -1,3 +1,30 @@
 """
+Вычислить число Пи c заданной точностью d
 
+Пример:
+
+- при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 """
+
+
+def calculate_pi(d):
+
+    pi = 3
+    counter = 0
+    i = 2
+    dif = 1
+    while dif > float(d):
+        my_list = list()
+        my_list.append(4 / (i * (i + 1) * (i + 2)))
+        my_list.append(4 / ((i + 2) * (i + 3) * (i + 4)))
+        dif = abs(my_list[0] - my_list[1])
+        pi += my_list[0] - my_list[1]
+        counter += 1
+        i += 4
+        print(counter, pi)
+    pi_txt = str(pi)
+    print(d, len(d))
+    print(pi_txt[:len(d)])
+
+
+calculate_pi('0.000001')
