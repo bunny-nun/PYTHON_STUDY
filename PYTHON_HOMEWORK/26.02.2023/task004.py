c9bc6fe376a10038c3bcaa1371bfda4f9f9c948c
Valeria <nelera.gb@gmail.com>
2023-02-26 16:59:08 +0300

    Задание 4: Преобразовать слова «разработка», «администрирование», «protocol», «standard» из строкового представления в байтовое и выполнить обратное преобразование (используя методы encode и decode).

--- PYTHON_HOMEWORK/26.02.2023/task004.py
+++ PYTHON_HOMEWORK/26.02.2023/task004.py
@@ -7,3 +7,11 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+
+my_list = ['разработка', 'администрирование', 'protocol', 'standard']
+
+for i in my_list:
+    i = i.encode(encoding='UTF-8')
+    print(i, type(i))
+    i = i.decode(encoding='UTF-8')
+    print(i, type(i))
